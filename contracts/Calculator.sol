9e9cca938d581d30dd4a5b33061df88fc31933a6
Adekunle Stephen Omorotimi <okste1234@gmail.com>
2024-01-17 00:38:35 +0100

    Calc Contract initialized

---
+++ contracts/Calculator.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Calculator {
+    function add(int _a, int _b) public pure returns (int) {
+        return _a + _b;
+    }
+}
